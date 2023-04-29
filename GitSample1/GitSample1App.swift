e375888eb882d1a3af0bd2ca316a754295ae1ecb
muranakar <p1211065@gmail.com>
2023-04-29 10:35:29 +0900

    first commit

---
+++ GitSample1/GitSample1App.swift
@@ -0,0 +1,17 @@
+//
+//  GitSample1App.swift
+//  GitSample1
+//
+//  Created by 村中令 on 2023/04/29.
+//
+
+import SwiftUI
+
+@main
+struct GitSample1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
